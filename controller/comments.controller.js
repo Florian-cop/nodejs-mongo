8e9e9227aae577ea2df884341776c1e5e56eb956
Florian Cop <floriancopitet@gmail.com>
2025-04-04 16:16:32 +0200

    Fix: comments

--- controller/comments.controller.js
+++ controller/comments.controller.js
@@ -1,7 +1,7 @@
 const Comment = require("./../model/comments.model.js");
 
 exports.addComments = async (req, res) => {
-    if (!req.body.text || req.body.text === "") {
+    if (!req?.body?.text || req?.body?.text === "") {
         return res.status(400).json({ message: "Veuillez saisir votre commentaires" })
     };
     let comments = await Comment.create({
@@ -19,18 +19,17 @@ exports.getById = async (req, res) => {
 }
 
 exports.update = async (req, res) => {
-    let comments = await Comment.findOne({ _id: req._id });
-
-    if (!comments) {
+    let comment = await Comment.findOne({ _id: req.params.id });
+    if (!comment) {
         return res.status(404).json({ error: "Commentaire non trouvé" });
     }
     if (req.body.text) {
-        comments.text = req.body.text;
+        comment.text = req.body.text;
     }
 
     try {
-        await comments.save();
-        return res.status(201).json(comments);
+        await comment.save();
+        return res.status(201).json(comment);
     }catch(e){
         return res.status(500).json({error: "Problème lors de la modificaition du commentaire"});
     }
@@ -38,7 +37,8 @@ exports.update = async (req, res) => {
 
 exports.delete = async (req, res) => {
     let result = await Comment.deleteOne({ _id: req.params.id });
-    if (result !== 1) {
+    console.log("🚀 ~ exports.delete ~ result:", result)
+    if (result.deletedCount !== 1) {
         return res.status(404).json({ error: "commentaire non trouvé" });
     }
     return res.status(200).json({ message: "commentaire supprimé" });
