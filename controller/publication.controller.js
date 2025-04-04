5a430b59f99ed5802adf2fe6c1bb1f55053b3848
El-Chaher Ahamadi Feti <@elchaher.ahamadifeti.etu>
2025-04-04 15:05:38 +0200

    ajout fonctionnalité Gestion des publications

---
+++ controller/publication.controller.js
@@ -0,0 +1,47 @@
+const Publication = require('../model/publication.model');
+
+exports.createPublication = async (req, res) => {
+    const newPublication = await Publication.create({ _text: req.body });
+    if (!newPublication) {
+        return res.status(400).json({ error: "Error creating publication" });
+    }   
+    res.status(201).json(newPublication);
+}
+
+exports.getAllPublications = async (req, res) => {
+    const publications = await Publication.find();
+    if(!publications) {
+        return res.status(404).json({ error: "Publications not found" });
+    }
+    return res.status(200).json(publications);
+}
+
+exports.getPublicationById = async (req, res) => {
+    const publication = await Publication.findById( {_id: req.params.id} );
+    if(!publication) {
+        return res.status(404).json({ error: "Publication not found" });
+    }
+    return res.status(200).json(publication);
+}
+
+exports.updatePublication = async (req, res) => { 
+    const publication = await Publication.findById( {_id: req.params.id} );
+    if(!publication) {
+        return res.status(404).json({ error: "Publication not found" });
+    }
+    if(!req.body.texte) {
+        return res.status(400).json({ error: "Text is required" });
+    }
+    if(req.body.texte === "") {
+        return res.status(400).json({ error: "Text is required" });
+    }
+    return res.status(200).json({ message: "Publication updated successfully" });
+}
+
+exports.deletePublication = async (req, res) => {
+    const publication = await Publication.findByIdAndDelete({ _id: req.params.id });
+    if(!publication) {
+        return res.status(404).json({ error: "Publication not found" });
+    }
+    return res.status(200).json({ message: "Publication deleted successfully" });
+}
