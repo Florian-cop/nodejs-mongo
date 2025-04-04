ea6b14da2eb1b28dca170c10801bf7bde61e26a2
Florian Cop <floriancopitet@gmail.com>
2025-04-04 15:13:18 +0200

    Feat: Add comments

--- database/connection.js
+++ database/connection.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 const connect = async () => {
     try {
-        await mongoose.connect('mongodb://127.0.0.1:27017/mongodb-g5');
+        await mongoose.connect('mongodb://127.0.0.1:27017/db-projet-mongo');
         console.log("connected to database");
     } catch (error) {
         console.error("Error database connection" + error);
